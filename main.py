8ac10ec33f22092b23c80c5d4bfd3d4faef64c79
unknown <hydham007@gmail.com>
2023-03-12 10:25:47 +0530

    updated search feature

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI, Query, Path, HTTPException, status, Body, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import FastAPI, Query, Path, HTTPException, status, Body, Request, Form
 from fastapi.encoders import jsonable_encoder
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel, Field
 from typing import Optional, List, Dict
-
+from starlette.responses import HTMLResponse
+from starlette.status import HTTP_400_BAD_REQUEST
 from database import cars
 
 templates = Jinja2Templates(directory="templates")
@@ -23,31 +24,33 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/", response_class= HTMLResponse)
+@app.get("/", response_class=RedirectResponse)
 def root(request: Request):
     return RedirectResponse(url="/cars")
-    
 
 @app.get("/cars", response_class=HTMLResponse)
 def get_cars(request: Request, number: Optional[str] = Query("10",max_length=3)):
     response = []
     for id, car in list(cars.items())[:int(number)]:
-        to_add = {}
-        to_add[id] = car
-        response.append(to_add)
-    return templates.TemplateResponse("index.html", {"request": request, "title": "Cars", "cars": response})
+        response.append((id,car))
+    return templates.TemplateResponse("index.html", {"request": request, "cars": response, "title": "Home"})
 
-@app.get("/cars/{id}", response_model=Car)
-def get_car_by_id(id: int = Path(...,ge=0,lt=1000)):
+@app.post("/search", response_class=RedirectResponse)
+def search_cars(id: str = Form(...)):
+    return RedirectResponse("/cars/" + id, status_code=302)
+
+@app.get("/cars/{id}", response_class=HTMLResponse)
+def get_car_by_id(request: Request, id: int = Path(...,ge=0,lt=1000)):
     car = cars.get(id)
+    response = templates.TemplateResponse("search.html", {"request": request, "car": car, "id": id, "title": "Search Car"})
     if not car:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Could not find car by ID.")
-    return car
+        response.status_code = status.HTTP_404_NOT_FOUND
+    return response
 
 @app.post("/cars", status_code=status.HTTP_201_CREATED)
 def add_cars(body_cars: List[Car], min_id: Optional[int] = Body(0)):
     if len(body_cars) < 1:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail="No cars to add.")
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST,detail="No cars to add.")
     min_id = len(cars.values()) + min_id
     for car in body_cars:
         while cars.get(min_id):
@@ -55,23 +58,21 @@ def add_cars(body_cars: List[Car], min_id: Optional[int] = Body(0)):
         cars[min_id] = car
         min_id += 1
 
-# update car details using PUT - http method with ID passed as Path Parameter and new fields as Request Body
-@app.put("/cars/{id}", response_model=Dict[str, Car])
+@app.put("/cars/{id}",response_model=Dict[str,Car])
 def update_car(id: int, car: Car = Body(...)):
     stored = cars.get(id)
     if not stored:
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail= "Car ID not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Could not find car with given ID.")
     stored = Car(**stored)
     new = car.dict(exclude_unset=True)
     new = stored.copy(update=new)
-    car[id] = jsonable_encoder(new)
+    cars[id] = jsonable_encoder(new)
     response = {}
-    response[id] = car[id]
+    response[id] = cars[id]
     return response
 
-# delete a car from database - car_id passed as path parameter in the URL
-@app.delete("/cars/{id}", status_code=status.HTTP_200_OK)
+@app.delete("/cars/{id}")
 def delete_car(id: int):
     if not cars.get(id):
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail= "Car ID not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Could not find car with given ID.")
     del cars[id]
